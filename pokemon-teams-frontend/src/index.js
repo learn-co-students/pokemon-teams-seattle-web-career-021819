09e51979d474bbd63a512a9f4fec7767b5f5cc24
bbpak <brianbpak@gmail.com>
2019-04-04 14:41:03 -0700

    rerender by part

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -16,7 +16,7 @@ function getTrainers() {
 }
 
 // ADD POKEMON
-function addPokemon(id) {
+function getPokemon(id) {
   fetch(POKEMONS_URL, {
     method: "POST",
     headers: {
@@ -29,7 +29,7 @@ function addPokemon(id) {
   .then(res => res.json())
   .then(
     pokemon => {
-      rerender()
+      addPokemon(pokemon)
     }
   )
 }
@@ -42,23 +42,28 @@ function deletePokemon(id) {
       'Content-Type': 'application/json'
     }
   })
-  .then( res => {
-    rerender()
-  })
+  .then(res => res.json())
+  .then(
+    pokemon => {
+      removePokemon(pokemon)
+    }
+  )
 }
 
-function rerender() {
-  // Delete cards
-  while (main.firstChild)
-    main.firstChild.remove()
+function addPokemon(pokemon) {
+  ul = document.getElementById("trainer"+pokemon.trainer_id)
+  ul.appendChild(createPokemon(pokemon))
+}
 
-  // Render cards with updated data
-  getTrainers()
+function removePokemon(pokemon) {
+  li = document.getElementById("pokemon"+pokemon.id)
+  li.remove()
 }
 
 function createPokemon(pokemon) {
   li = document.createElement('li')
   li.textContent = `${pokemon.nickname} (${pokemon.species})`
+  li.setAttribute('id', "pokemon"+pokemon.id)
 
   button = document.createElement('button')
   button.textContent = 'Release'
@@ -80,19 +85,19 @@ function renderCards(data) {
 function createTrainer(trainer) {
   card = document.createElement('div')
   card.classList.add("card")
-  card.setAttribute('data-id', trainer.id)
 
   add = document.createElement('button')
   add.textContent = 'Add Pokemon'
   add.setAttribute('data-trainer-id', trainer.id)
   add.addEventListener('click', () => {
-    addPokemon(trainer.id)
+    getPokemon(trainer.id)
   })
   
   p = document.createElement('p')
   p.textContent = trainer.name
 
   ul = document.createElement('ul')
+  ul.setAttribute('id', "trainer" + trainer.id)
 
   trainer.pokemons.forEach(pokemon => {
     ul.appendChild(createPokemon(pokemon))
